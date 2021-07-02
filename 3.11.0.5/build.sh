adfdc1e04526a8c0adac495b25a18acf5e4e9c6f
halbakri <84340692+halbakri@users.noreply.github.com>
2021-07-02 13:25:08 -0400

    Update build.sh

--- 3.11.0.5/build.sh
+++ 3.11.0.5/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Copyright © 2021 Pittsburgh Supercomputing Center.
+# All Rights Reserved.
+
 IMAGE=singularity-aspera-connect-3.11.0.5.sif
 DEFINITION=Singularity
 
@@ -7,9 +10,11 @@ if [ -f $IMAGE ]; then
 	rm -fv $IMAGE
 fi
 
-tar -xf ibm-aspera-connect-3.11.0.5-linux-g2.12-64.tar.gz
-./ibm-aspera-connect-3.11.0.5-linux-g2.12-64.sh
-
 sudo singularity build $IMAGE $DEFINITION
 
-rm -f ibm-aspera-connect-3.11.0.5-linux-g2.12-64.sh
+if [ -f $IMAGE ]; then
+	exit 0
+else
+	exit 1
+fi
+
