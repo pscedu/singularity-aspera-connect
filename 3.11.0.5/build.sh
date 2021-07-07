dd86fd3623f5a671b7a455461db00491d6395629
icaoberg <icaoberg@andrew.cmu.edu>
2021-07-07 16:35:53 -0400

    Update build.sh

--- 3.11.0.5/build.sh
+++ 3.11.0.5/build.sh
@@ -10,7 +10,10 @@ if [ -f $IMAGE ]; then
 	rm -fv $IMAGE
 fi
 
+tar -xf ibm-aspera-connect-3.11.0.5-linux-g2.12-64.tar.gz
+./ibm-aspera-connect-3.11.0.5-linux-g2.12-64.sh
 sudo singularity build $IMAGE $DEFINITION
+rm -f ibm-aspera-connect-3.11.0.5-linux-g2.12-64.sh
 
 if [ -f $IMAGE ]; then
 	exit 0
